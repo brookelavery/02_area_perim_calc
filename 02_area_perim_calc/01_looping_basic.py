1cf511c797c3489a7eaf1064965c30a6e5000479
brookelavery <101751141+brookelavery@users.noreply.github.com>
2022-04-06 10:25:47 +1200

    Add files via upload

--- 02_area_perim_calc/01_looping_basic.py
+++ 02_area_perim_calc/01_looping_basic.py
@@ -1,15 +1,12 @@
-from urllib import response
-
-
 valid = False
 while not valid:
+    response = float(input("enter a number"))
 
-response = float(input("Enter a number: "))
+    if response > 0:
+        valid = True 
 
-if response > 0:
-    valid = True
 
-else:
-    print("please enter a number that is more than zero")
-    print()
-
+    else:
+        print("please enter a number that is more than zero")
+        print()
+             
